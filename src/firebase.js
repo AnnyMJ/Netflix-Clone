1131e0285274abbbd3ab550fc986b9211d206ba0
AnnyMJ <annymorontajimenez@gmail.com>
2022-10-11 15:54:49 -0400

    setting up project - homescreen

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqR4swhtDmm6XkFmt86OQicISIE4u1dDQ",
+    authDomain: "netflix-clone-77c5d.firebaseapp.com",
+    projectId: "netflix-clone-77c5d",
+    storageBucket: "netflix-clone-77c5d.appspot.com",
+    messagingSenderId: "621991871748",
+    appId: "1:621991871748:web:ef40f2f20855991c6bbc51"
+  };
+  
---
+++ src/pages/HomeScreen/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../../components/Nav';
+import './index.css';
+
+function HomeScreen () {
+  return (
+    <div className="homescreen">
+      <Nav/>
+    </div>
+  );
+}
+
+export default HomeScreen;
