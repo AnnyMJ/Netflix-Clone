349fb5fb6d32e32781e3a9dfb69d168e2b9d8414
AnnyMJ <annymorontajimenez@gmail.com>
2022-10-13 10:40:56 -0400

    banner component

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react"; 
-import './index.scss';
 
-const Nav = () => {
+import './style.scss';
+
+function Nav () {
 
     const [show, handleShow] = useState(false);
 
--- src/pages/HomeScreen/index.jsx
+++ src/pages/HomeScreen/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Banner from '../../components/Banner';
 import Nav from '../../components/Nav';
+
 import './index.css';
 
 function HomeScreen () {
   return (
     <div className="homescreen">
       <Nav/>
+      <Banner/>
     </div>
   );
 }
